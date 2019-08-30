3ce7f131024b35f4856cd0835aa2c8ff7cb30792
ayj412 <ayj412@naver.com>
2019-08-31 00:04:52 +0900

    회원가입 로그인페이지 구현

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -8,6 +8,7 @@ import Main from './component/page/Main'
 import Mypage from './component/page/Mypage'
 import Ranking from './component/page/Ranking'
 import Login from './component/page/Login';
+import Regist from './component/page/Regist';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/mypage/" component={Mypage} />
         <Route path="/ranking/" component={Ranking} />
         <Route path="/login/" component={Login} />
+        <Route path="/Regist/" component={Regist} />
         <Route path='/github' component={() => { window.location = 'https://github.com/h4ppyy/jlpt-battle'; return null;} }/>
         <Footer/>
       </Router>
