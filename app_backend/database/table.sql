2ac8891d820d37be15a99eaee63c2a09b958f962
h4ppyy <93immm@naver.com>
2019-09-04 13:18:12 +0900

    table.sql update

--- app_backend/database/table.sql
+++ app_backend/database/table.sql
@@ -7,17 +7,6 @@ use test;
 insert into tbl_user(username, password, jlpt_level)
 value('admin', 'password', 3);
 
-insert into tbl_japan_store(kanji, hiragana, hangul, type, level)
-values('敢えて', 'あえて', '감히', '부사', 1);
-insert into tbl_japan_store(kanji, hiragana, hangul, type, level)
-values('愛想', 'あいそう', '애상', '명사', 1);
-insert into tbl_japan_store(kanji, hiragana, hangul, type, level)
-values('合間', 'あいま', '틈', '명사', 1);
-insert into tbl_japan_store(kanji, hiragana, hangul, type, level)
-values('赤らむ', 'あからむ', '붉어지다', '부사', 1);
-insert into tbl_japan_store(kanji, hiragana, hangul, type, level)
-values('悪', 'あく', '악', '명사동사', 1);
-
 -- 회원 테이블
 create table tbl_user(
 	id int primary key auto_increment,
@@ -33,29 +22,93 @@ create table tbl_user(
 	delete_date datetime default null
 ) default character set utf8 collate utf8_general_ci;
 
-create table tbl_japan_store(
-	id int primary key auto_increment,
-    kanji varchar(255) comment '한자',
-    hiragana varchar(255) comment '히라가나',
-    hangul varchar(255) comment '한글',
-    type varchar(255) comment '명사, 동사, 부사, 형용사',
-    level int not null comment 'jlpt 1, 2, 3, 4, 5',
-    exam_yn varchar(255) default 'N' comment '출제 Y, 미출제 N',
-    delete_yn varchar(1) default 'N',
-	regist_date datetime default now(),
-	modify_date datetime default null,
-	delete_date datetime default null
+-- 문제테이블 (N1)
+CREATE TABLE `tbl_problem_n1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) default character set utf8 collate utf8_general_ci;
 
--- 문제 테이블
-create table tbl_japan_problem(
-	id int primary key auto_increment,
-    store_id int,
-    user_id int,
-    delete_yn varchar(1) default 'N',
-	regist_date datetime default now(),
-	modify_date datetime default null,
-	delete_date datetime default null
+-- 문제테이블 (N2)
+CREATE TABLE `tbl_problem_n2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) default character set utf8 collate utf8_general_ci;
+
+-- 문제테이블 (N3)
+CREATE TABLE `tbl_problem_n3` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) default character set utf8 collate utf8_general_ci;
+
+-- 문제테이블 (N4)
+CREATE TABLE `tbl_problem_n4` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) default character set utf8 collate utf8_general_ci;
+
+-- 문제테이블 (N5)
+CREATE TABLE `tbl_problem_n5` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) default character set utf8 collate utf8_general_ci;
+
+-- 문제테이블 (자유)
+CREATE TABLE `tbl_problem_free` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `store_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) default character set utf8 collate utf8_general_ci;
+
+-- 한자 보관 테이블
+CREATE TABLE `tbl_japan_store` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kanji` varchar(255) DEFAULT NULL COMMENT '한자',
+  `hiragana` varchar(255) DEFAULT NULL COMMENT '히라가나',
+  `hangul` varchar(255) DEFAULT NULL COMMENT '한글',
+  `type` varchar(255) DEFAULT NULL COMMENT '명사, 동사, 부사, 형용사',
+  `level` int(11) NOT NULL COMMENT 'jlpt 1, 2, 3, 4, 5',
+  `check_free_yn` varchar(255) DEFAULT 'N' COMMENT '출제 Y, 미출제 N / 자유서버',
+  `check_each_yn` varchar(255) DEFAULT 'N' COMMENT '출제 Y, 미출제 N / 각 난이도서버',
+  `delete_yn` varchar(1) DEFAULT 'N',
+  `regist_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `modify_date` datetime DEFAULT NULL,
+  `delete_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) default character set utf8 collate utf8_general_ci;
 
 -- 채팅 테이블
