8d0bc2a4923521a45793b78247e63b6c158020e3
h4ppyy <93immm@naver.com>
2019-09-05 16:01:49 +0900

    문제제출 테스트 스크립트 작성

--- app_backend/controllers/getHistoryLog.js
+++ app_backend/controllers/getHistoryLog.js
@@ -8,18 +8,18 @@ exports.getHistoryLog = function(req, res) {
     const connection = mysql.createConnection(dbconfig);
     const level = req.body.level;
     const sql = "SELECT Ifnull(y.username, 'PC') AS username, "+
-              "Ifnull(x.modify_date, '정답 미제출로 인한 pass') AS modify_date, "+
-              "z.kanji, "+
-              "z.hiragana, "+
-              "z.hangul "+
-              "FROM   tbl_problem_"+level+" x "+
-              "LEFT JOIN tbl_user y "+
-              "ON x.user_id = y.id "+
-              "JOIN tbl_japan_store z "+
-              "ON x.store_id = z.id "+
-              "WHERE  x.regist_date > Date_format(Date_sub(Now(), INTERVAL "+common.SLOW_QUERY_SOLUTION+" day), '%Y-%m-%d') "+
-              "ORDER  BY x.regist_date DESC "+
-              "LIMIT  11; "
+                "Ifnull(x.modify_date, '0000-00-00 00:00:00') AS modify_date, "+
+                "z.kanji, "+
+                "z.hiragana, "+
+                "z.hangul "+
+                "FROM   tbl_problem_"+level+" x "+
+                "LEFT JOIN tbl_user y "+
+                "ON x.user_id = y.id "+
+                "JOIN tbl_japan_store z "+
+                "ON x.store_id = z.id "+
+                "WHERE  x.regist_date > Date_format(Date_sub(Now(), INTERVAL "+common.SLOW_QUERY_SOLUTION+" day), '%Y-%m-%d') "+
+                "ORDER  BY x.regist_date DESC "+
+                "LIMIT  11; "
     common.logging_debug('sql', sql);
     connection.query(sql, function(err, rows, fields) {
       if (err == null) {
